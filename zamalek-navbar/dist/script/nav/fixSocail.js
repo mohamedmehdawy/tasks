3e05ea596e1ed65b1fa5fde34b3fbc7ae61a3449
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-07-26 01:05:30 +0200

    fix some things in ar

--- zamalek-navbar/dist/script/nav/fixSocail.js
+++ zamalek-navbar/dist/script/nav/fixSocail.js
@@ -4,6 +4,5 @@
 function fixSocail() {
     let nav = document.querySelector("nav#main-nav"), socail = nav.querySelector(".links .top .left .socail"), links = nav.querySelector(".bottom ul");
     socail.style.width = getComputedStyle(links.children[links.children.length - 1]).width;
-    console.log("hello");
 }
 export default fixSocail;
--- zamalek-navbar/project/script/nav/fixSocail.ts
+++ zamalek-navbar/project/script/nav/fixSocail.ts
@@ -8,6 +8,5 @@ function fixSocail() {
         links = nav.querySelector(".bottom ul") as HTMLUListElement;
 
     socail.style.width = getComputedStyle(links.children[links.children.length - 1]).width;
-    console.log("hello")
 }
 export default fixSocail; 
